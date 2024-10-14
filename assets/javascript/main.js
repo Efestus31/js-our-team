e5900a5107fabf347f9eda57cacbb15ec693b229
Efestus31 <biagiohd@gmail.com>
2024-10-14 15:16:52 +0200

    add images add inner html add some styling

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,43 +3,45 @@ const teamMembers = [
         name: "Marco Bianchi",
         role: "Designer",
         email: "marcobianchi@team.com",
-        img: "img/male1.png"
+        img: "./assets/img/male1.png"
     },
     {
         name: "Laura Rossi",
         role: "Front-end Developer",
         email: "laurarossi@team.com",
-        img: "img/female1.png"
+        img: "./assets/img/female1.png"
     },
     {
         name: "Giorgio Verdi",
         role: "Back-end Developer",
         email: "giorgioverdi@team.com",
-        img: "img/male2.png"
+        img: "./assets/img/male2.png"
     },
     {
         name: "Marta Ipsum",
         role: "SEO Specialist",
         email: "martarossi@team.com",
-        img: "img/female2.png"
+        img: "./assets/img/female2.png"
     },
     {
         name: "Roberto Lorem",
         role: "SEO Specialist",
         email: "robertolorem@team.com",
-        img: "img/male3.png"
+        img: "./assets/img/male3.png"
     },
     {
         name: "Daniela Amet",
         role: "Analyst",
         email: "danielaamet@team.com",
-        img: "img/female3.png"
+        img: "./assets/img/female3.png"
     }
 ];
 
 //selezioniamo gli elementi nella DOM
 const teamEl = document.getElementById('team');
 
+
+let definitiveMarkup = '';
 //Elaborazione dati
 for (let i = 0; i < teamMembers.length; i++) {
 
@@ -47,6 +49,7 @@ for (let i = 0; i < teamMembers.length; i++) {
 
     const markup = generate_team_member_card(member)
 
+definitiveMarkup += markup;
 
 }
 
@@ -57,7 +60,7 @@ function generate_team_member_card(member) {
 
     return `
     <div class="col">
-            <div class="card" style="width: 18rem;">
+            <div class="card mb-5" style="width: 18rem;">
                 <img src="${img}" class="card-img-top" alt="Team Member picture">
                 <div class="card-body">
                     <div class="text-section">
@@ -70,3 +73,5 @@ function generate_team_member_card(member) {
      </div>
     `
 }
+
+teamEl.innerHTML = definitiveMarkup
